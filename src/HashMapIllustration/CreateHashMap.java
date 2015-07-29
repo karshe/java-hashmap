75a6dea9ce1c7470929b1b3bea291cf69d1ef5b8
Utkarsh Kumar Raut <utkarsh2point0@gmail.com>
2015-07-29 09:25:53 +0530

    Created CreateHashMap.java class. Invoke Constructor for HashMap object creation.

---
+++ src/HashMapIllustration/CreateHashMap.java
@@ -0,0 +1,11 @@
+package HashMapIllustration;
+import java.util.HashMap;
+
+class CreateHashMap{
+    CreateHashMap(){
+        HashMap<String, Integer> mHashMap = new HashMap<>();
+        mHashMap.put("Shubham Singh", 23);
+        mHashMap.put("Gaurav Soni", 22);
+        mHashMap.put("Vikrant Singh", 21);
+    }
+}
