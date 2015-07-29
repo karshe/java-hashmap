d457e0449c9485627619fbd4b00bed97c332e1ed
Utkarsh Kumar Raut <utkarsh2point0@gmail.com>
2015-07-29 10:21:37 +0530

    Added sort(), remove() and size() for HashMap object.

--- src/HashMapIllustration/HashMapIllustration.java
+++ src/HashMapIllustration/HashMapIllustration.java
@@ -2,6 +2,7 @@ package HashMapIllustration;
 
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.TreeMap;
 
 /**
  *
@@ -15,8 +16,11 @@ public class HashMapIllustration {
         CreateHashMap cHashMap = new CreateHashMap();
         HashMap<String, Integer> mHashMap = cHashMap.populate();
         System.out.println("Added few entities.");
-        
+       
+        System.out.println("Size of mHashMap is : " + mHashMap.size());
         cHashMap.addStudent("Yash Tejas", 24);
+        System.out.println("Size of mHashMap after adding Yash Tejas is : " + mHashMap.size());
+        
         
         System.out.println("Retrieve age of Vikrant Singh");
         Integer mAge = mHashMap.get("Vikrant Singh");        
@@ -28,6 +32,42 @@ public class HashMapIllustration {
             String tName = keyItr.next();
             Integer tAge = mHashMap.get(tName);
             System.out.println("Age of "+tName+" is "+tAge);
+        }
+        
+        boolean ifYogesh;
+        
+        ifYogesh = mHashMap.containsKey("Yogesh Khanchandani");
+        System.out.println("Is Yogesh present? : " + ifYogesh);
+        
+        cHashMap.addStudent("Yogesh Khanchandani", 26);
+        
+        ifYogesh = mHashMap.containsKey("Yogesh Khanchandani");
+        System.out.println("Is Yogesh present? : " + ifYogesh);
+        
+        boolean ifOlder;
+        
+        ifOlder = mHashMap.containsValue(30);
+        System.out.println("Someone of 30? : " + ifOlder);
+        
+        String tName = "Yash Tejas";
+        Object tStudent = mHashMap.remove(tName);
+        System.out.println(tName+ " was removed!");
+        
+        System.out.println("Unsorted HashMap");
+        
+        keyItr = mHashMap.keySet().iterator();
+        while(keyItr.hasNext()){
+            tName = keyItr.next();
+            System.out.println(tName + " : " + mHashMap.get(tName));
+        }
+        
+        System.out.println("Sorting array using TreeMap");
+        TreeMap sortedMap = new TreeMap(mHashMap);
+        
+        keyItr = sortedMap.keySet().iterator();
+        while(keyItr.hasNext()){
+            tName = keyItr.next();
+            System.out.println(tName + " : " + sortedMap.get(tName));
         }
     }
 }
